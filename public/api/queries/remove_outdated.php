e774702479cade2ee10fba80937f26261918da4d
krischanthas <kris.chanthasiriphan@gmail.com>
2018-09-11 10:34:13 -0700

    changed removeOutdated function to run query when called

--- public/api/queries/remove_outdated.php
+++ public/api/queries/remove_outdated.php
@@ -1,9 +1,27 @@
 <?php
+require_once("../mysql_connect.php");
 
+
+// remove postings forom the jobs table that are outdated by -3 months 
 function removeOutdated($post_date){
+    $output = [
+        'success' => false,
+        'error' => []
+    ];
+    // gets current date and subtracts 3 months
     $datePostLimit = date("m/d/Y", strtotime("-3 months"));
     if($post_date < $datePostLimit){
-        return "DELETE FROM `jobs` WHERE $post_date < $datePostLimit";
+        $query = "DELETE FROM `jobs` WHERE $post_date < $datePostLimit";
+        $result = mysqli_query($conn, $query);
+
+        // checks results to see if any rows were affected
+        if(mysqli_affected_rows($conn) > 0){
+            $output['success'] = true;
+        }
+        else {
+            $output['error'][]= "Error deleting from jobs table where post is outdated";
+        }
+        
     }
 }
 
